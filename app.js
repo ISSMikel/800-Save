2bddc8bf48842f9401bad040a4ac6caa6a8ab4bd
Mikel Garcia <mikel.garcia@gov.tt>
2022-12-06 15:15:28 -0400

    commit

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
-  console.log('Test')
+ // console.log('Test')
 })
 
 // app.get('/save', function (req, res) {
-//    res.json({name:'Hello World'})
+//   res.json({name:'Hello World'})
 //   })
+
 app.listen(3000)
 console.log('Server is running on localhost:3000')
