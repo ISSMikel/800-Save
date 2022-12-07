6726859e7331d8cbd6454bd86560fa3e4605be00
Mikel Garcia <mikel.garcia@gov.tt>
2022-12-07 10:51:02 -0400

    2

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-Text
+const sql = require('mssql/msnodesqlv8');
+//database configuration
+var config={
+    user: 'sa',
+    password:'OPMserver1', 
+    database: '800_Save', // name of database 
+    server: 'WIN-T1K9LSOB69C',
+    driver: 'msnodesqlv8',
+    options:{
+        trustedConnection: true,
+        userName: 'Administrator',
+        password: 'OPMserver2'
+    }
+};
+
+
+// connect to database 
+
+sql.connect(config, function(err){
+    if (err){
+        console.log(err);
+    }
+    // create the request object 
+    var request = new sql.Request();
+    // database query
+    request .query('select * from Client', function(err,recordSet){
+       if(err){
+        console.log(err)
+       }else{
+        console.log(recordSet)
+       }
+    });  
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
+
+
